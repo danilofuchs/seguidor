47789228d597f09bf2e3b182777936b70a9948d1
Danilo Fuchs <danilo_fuchs@hotmail.com>
2019-09-14 09:03:09 -0300

    Rename variables

--- seguidor.ino
+++ seguidor.ino
@@ -1,40 +1,38 @@
-//itineráio:
-// - identificar a velocidade máxima e a velocidade mínima
-// - testar leitura do sensor
-// - identificar as constantes do PID
-
 #include <QTRSensors.h>
 QTRSensors qtr;
 
 #define motorA 10 //pino A1 ponte-h
+#define motorB 5  //pino B2 ponte-h
 #define IN1 9     //pino B1 ponte-h
 #define IN2 8     //pino B1 ponte-h
 #define IN3 7     //pino B1 ponte-h
 #define IN4 6     //pino B1 ponte-h
 #define STBY 3
-#define motorB 5 //pino B2 ponte-h
 
-#define ULTIMAS_PROPORCOES_COUNT 5
+#define SENSOR_COUNT 8
 
-float KD = 3, KP = 7, ERRO_LEITURA;
+#define PERIODO_AMOSTRAGEM 10
+#define ULTIMOS_PIDS_SIZE 20 // 100 / PERIODO_AMOSTRAGEM;
+
+#define VEL_MAX_LINEAR 200
+#define VELMAX 255
+
+float KD = 5, KP = 9, erroLeitura;
 // 5
-float KV = 0.7;
+float KV = 0.4;
 // 0.5
-float ERRO, ERRO_ANTERIOR = 0;
-float ULTIMAS_PROPORCOES[ULTIMAS_PROPORCOES_COUNT] = {0};
-int ULTIMAS_PROPORCOES_INDEX = -1;
-float CORRECAO_MIN_A, CORRECAO_MIN_B, CORRECAO_A, CORRECAO_B, CORRECAO;
-float VEL_MAX_LINEAR = 200;
-float VELMAX = 255;
+float erro, erroAnterior = 0;
+float ultimosPids[ULTIMOS_PIDS_SIZE] = {0};
+int ultimosPidsIndex = -1;
+float correcaoD, correcaoE, correcao;
 
 float maxProporcao = 300;
 
-const uint8_t SensorCount = 8;
-uint16_t sensorValues[SensorCount];
+uint16_t sensorValues[SENSOR_COUNT];
 
 unsigned long previousMillis = 0, currentMillis = 0;
 
-void setMotorA(int vel)
+void setMotorD(int vel)
 {
   if (vel >= 0)
   {
@@ -50,7 +48,7 @@ void setMotorA(int vel)
   }
 }
 
-void setMotorB(int vel)
+void setMotorE(int vel)
 {
   if (vel >= 0)
   {
@@ -68,22 +66,52 @@ void setMotorB(int vel)
 
 void acionaMotores(float velLinear, float velRadial)
 {
-  CORRECAO_A = velLinear - (velRadial * 0.5);
-  CORRECAO_B = velLinear + (velRadial * 0.5);
+  correcaoD = velLinear - (velRadial * 0.5);
+  correcaoE = velLinear + (velRadial * 0.5);
 
-  CORRECAO_A = constrain(CORRECAO_A, -VELMAX, VELMAX);
-  CORRECAO_B = constrain(CORRECAO_B, -VELMAX, VELMAX);
+  correcaoD = constrain(correcaoD, -VELMAX, VELMAX);
+  correcaoE = constrain(correcaoE, -VELMAX, VELMAX);
 
   // Serial.print(";   PID: ");
   // Serial.print(velRadial);
   // Serial.print(";   VelA: ");
-  // Serial.print(CORRECAO_A);
+  // Serial.print(correcaoD);
   // Serial.print(";   VelB: ");
-  // Serial.print(CORRECAO_B);
+  // Serial.print(correcaoE);
   // Serial.println(";");
 
-  setMotorA(CORRECAO_A);
-  setMotorB(CORRECAO_B);
+  setMotorD(correcaoD);
+  setMotorE(correcaoE);
+}
+
+void addItemUltimasProporcoes(float item)
+{
+  int numItems = ULTIMOS_PIDS_SIZE;
+  int newIndex = (ultimosPidsIndex + 1) % numItems;
+  ultimosPids[newIndex] = item;
+  ultimosPidsIndex = newIndex;
+}
+float getMediaUltimasProporcoes()
+{
+  int numItems = ULTIMOS_PIDS_SIZE;
+  float count = 0;
+  for (int i = 0; i < numItems; i++)
+  {
+    count += ultimosPids[i];
+  }
+  return count / numItems;
+}
+void printUltimasProporcoes()
+{
+  int numItems = ULTIMOS_PIDS_SIZE;
+  Serial.print("[");
+  for (int i = 0; i < numItems; i++)
+  {
+    int index = (ultimosPidsIndex + i) % numItems;
+    Serial.print(ultimosPids[index]);
+    Serial.print(",");
+  }
+  Serial.print("]\n");
 }
 
 void setup()
@@ -100,77 +128,32 @@ void setup()
 
   qtr.setTypeRC();
   const uint8_t sensorPins[] = {A0, A1, A2, A3, A4, A5, 2, 4};
-  qtr.setSensorPins(sensorPins, SensorCount);
+  qtr.setSensorPins(sensorPins, SENSOR_COUNT);
   for (uint16_t i = 0; i < 200; i++)
   {
     qtr.calibrate(); //calibração do sensor;
   }
 
   digitalWrite(STBY, 1);
-
-  CORRECAO_MIN_A = 80;
-  CORRECAO_MIN_B = 82; // VELOICIDADE CERTA E 72
-}
-
-void addItemUltimasProporcoes(float item)
-{
-  int numItems = ULTIMAS_PROPORCOES_COUNT;
-  int newIndex = (ULTIMAS_PROPORCOES_INDEX + 1) % numItems;
-  ULTIMAS_PROPORCOES[newIndex] = item;
-  ULTIMAS_PROPORCOES_INDEX = newIndex;
-}
-float getMediaUltimasProporcoes()
-{
-  int numItems = ULTIMAS_PROPORCOES_COUNT;
-  float count = 0;
-  for (int i = 0; i < numItems; i++)
-  {
-    count += ULTIMAS_PROPORCOES[i];
-  }
-  return count / numItems;
-}
-void printUltimasProporcoes()
-{
-  int numItems = ULTIMAS_PROPORCOES_COUNT;
-  Serial.print("[");
-  for (int i = 0; i < numItems; i++)
-  {
-    int index = (ULTIMAS_PROPORCOES_INDEX + i) % numItems;
-    Serial.print(ULTIMAS_PROPORCOES[index]);
-    Serial.print(",");
-  }
-  Serial.print("]\n");
 }
 
 void loop()
 {
   currentMillis = millis();
-  if (currentMillis - previousMillis >= 50)
+  if (currentMillis - previousMillis >= PERIODO_AMOSTRAGEM)
   {
     previousMillis = currentMillis;
     int position = qtr.readLineWhite(sensorValues);
     // position: de 0 a 7000
-    ERRO = 3500 - position;
+    erro = 3500 - position;
     // erro: de -3500 a 3500
-    ERRO_LEITURA = position / 3500.0 - 1.0;
+    erroLeitura = position / 3500.0 - 1.0;
     // erro_leitura: de -1 a 1
     // kp: de 1 a 10
     // kd: de 1 a 10
-    float PID = KP * ERRO_LEITURA + KD * (ERRO_LEITURA - ERRO_ANTERIOR);
+    float PID = KP * erroLeitura + KD * (erroLeitura - erroAnterior);
     // PID: da ordem 1 a 10 (vezes o original)
-    ERRO_ANTERIOR = ERRO_LEITURA;
-
-    // Serial.println(CORRECAO);
-
-    // if (abs(CORRECAO) > maxProporcao)
-    // {
-    //   maxProporcao = abs(CORRECAO);
-    // }
-    // CORRECAO *= 100.0 / maxProporcao;
-    // CORRECAO = constrain(CORRECAO, -100, 100);
-
-    Serial.println("novo");
-    Serial.println(CORRECAO);
+    erroAnterior = erroLeitura;
 
     addItemUltimasProporcoes(PID);
     float media = getMediaUltimasProporcoes();
@@ -178,9 +161,9 @@ void loop()
     float compensacaoVelocidadeLinear = constrain(1.0 - (abs(media) / 100.0) * KV, 0, 1);
     float velocidadeLinear = VEL_MAX_LINEAR * compensacaoVelocidadeLinear;
 
-    CORRECAO = PID * velocidadeLinear;
+    correcao = PID * velocidadeLinear;
 
-    if (ERRO_LEITURA == 1 || ERRO_LEITURA == -1)
+    if (erroLeitura == 1 || erroLeitura == -1)
     {
       // Fora da pista
       if (media > 0)
@@ -196,19 +179,21 @@ void loop()
     }
     else
     {
-      acionaMotores(velocidadeLinear, CORRECAO);
+      acionaMotores(velocidadeLinear, correcao);
     }
 
-    /*Serial.print("Sensor: ");
-      Serial.print(position);
-      Serial.print("    Erro: ");
-      Serial.print(ERRO_LEITURA);
-      Serial.print(";   PID: ");
-      Serial.print(CORRECAO);
-      Serial.print(";   VelA: ");
-      Serial.print(CORRECAO_A);
-      Serial.print(";   VelB: ");
-      Serial.print(CORRECAO_B);
-      Serial.println(";");*/
+    // Serial.print("Sensor: ");
+    // Serial.print(position);
+    // Serial.print(" Erro: ");
+    // Serial.print(erroLeitura);
+    // Serial.print("; PID: ");
+    // Serial.print(PID);
+    // Serial.print("; Comp. Vel: ");
+    // Serial.print(compensacaoVelocidadeLinear);
+    // Serial.print("; VelA: ");
+    // Serial.print(correcaoD);
+    // Serial.print("; VelB: ");
+    // Serial.print(correcaoE);
+    // Serial.println(";");
   }
 }
